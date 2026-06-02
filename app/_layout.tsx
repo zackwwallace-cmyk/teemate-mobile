8b0971cbe47dc43b800c73e01b3882881f2a21df
zackwwallace-cmyk <zackwwallace@gmail.com>
2026-06-02 19:20:12 -0400

    Register password reset screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -56,6 +56,8 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false, animation: 'none' }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="auth" />
+        <Stack.Screen name="forgot-password" />
+        <Stack.Screen name="reset-password" />
         <Stack.Screen name="onboarding" />
         <Stack.Screen name="rounds" />
         <Stack.Screen name="discover" />
